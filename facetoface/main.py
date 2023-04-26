d2ed51291bbaed9a4a0c005ef74a6b89605b1417
Matheus Aprigio <126816425+MatheusAprigio11@users.noreply.github.com>
2023-04-26 16:58:11 -0300

    Create main.py

---
+++ facetoface/main.py
@@ -0,0 +1,7 @@
+
+class Fruits:
+    def __init__(self, name, t1, t2, t3, t4, t5):        
+        self.name = name
+        self.tips = [t1, t2, t3, t4, t5]
+
+banana = Fruits('Banana,  ')        
