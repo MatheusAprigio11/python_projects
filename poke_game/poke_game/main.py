5acca037eae1077b69676ab5e23ec0bd0ddb2cf7
Matheus Aprigio <126816425+MatheusAprigio11@users.noreply.github.com>
2023-05-17 17:00:02 -0300

    Update main.py

--- poke_game/poke_game/main.py
+++ poke_game/poke_game/main.py
@@ -1,70 +1,109 @@
 import random
-
+import inquirer
 class Pokemon:
     """
     Classe mãe que contem as contem as caracteristiscas do pokemon
     """
-    def __init__(self, name, life, attack, velocity):
+    def __init__(self, name, life, damage, velocity, mov):
         self.__name = name
-        self.__life = life
-        self.__attack = attack
+        self.life = life
+        self.__damage = damage
         self.__velocity = velocity
+        self.__mov = mov
 
     @property
     def name(self):
         return self.__name
     
-    @property
-    def life(self):
-        return self.__life
+    # @property
+    # def life(self):
+    #     return self.__life
     
     @property
-    def attack(self):
-        return self.__attack
+    def damage(self):
+        return self.__damage
     
     @property
     def velocity(self):
         return self.__velocity
     
+    @property
+    def mov(self):
+        return self.__mov
+    
+    def choose_mov(self):
+        move_selected = inquirer.prompt([
+            inquirer.List('moves',
+                        message="||SELECT YOUR ATTACK BRO||",
+                        choices=[self.mov],
+                        ),
+        ])
+        return move_selected['moves']
+
 
 class Fire(Pokemon):
     """
     Classe filha, que está herdando de Pokemon e lhe dando um elemento
     """
-    def __init__(self, name, life, attack, velocity, type):
-        super().__init__(name , life, attack, velocity)
+    def __init__(self, name, life, damage, velocity, mov, type):
+        super().__init__(name , life, damage, velocity, mov)
         self.tpye = type
 
-    def advantage(self, type):
+    @staticmethod
+    def advantage(type, damage):
         if type == "Water":
-            return False
-        elif type == "Plaint":
-            return True
+            damage = damage*0.10
+            return damage
         else:
-            ... #RANDINT
+            return damage
+
+    def attack(self, bot_type, damage):
+        damage = self.advantage(bot_type, damage)
+        damage = self.choose_mov()
+        return damage
 
 class Plaint(Pokemon):
     """
     Classe filha, que está herdando de Pokemon e lhe dando um elemento
     """
-    def __init__(self, name, life, attack, velocity, type):
-        super().__init__(name , life, attack, velocity)
+    def __init__(self, name, life, damage, velocity, mov, type):
+        super().__init__(name , life, damage, velocity, mov)
         self.type = type
+
+    @staticmethod
+    def advantage(type, damage):
+        if type == "Fire":
+            return False
+        elif type == "Water": #grass
+            damage = damage*0.10
+            return damage
+        else:
+            return damage
 
 class Water(Pokemon):
     """
     Classe filha, que está herdando de Pokemon e lhe dando um elemento
     """
-    def __init__(self, name, life, attack, velocity, type):
-        super().__init__(name , life, attack, velocity)
+    def __init__(self, name, life, damage, velocity, mov, type):
+        super().__init__(name , life, damage, velocity, mov)
         self.type = type
 
+    @staticmethod
+    def advantage(type, damage):
+        if type == "Plaint":
+            pass
+        elif type == "Fire": #grass
+            damage = damage*0.10
+            return damage
+        else:
+            return damage
 
-treeko = Plaint("Treeko", 40, 45, 70, "Plaint") #OBJETO CRIANDO POKEMON
-moltres = Fire("Moltres", 90, 100, 90, "Fire")
-lapras = Water("Lapras", 130, 85, 60, "Water")
 
-pokemons = [treeko.name, moltres.name, lapras.name]
+treeko = Plaint("Treeko", 40, 45, 70, "Bullet Seed", "Plaint") #OBJETO CRIANDO POKEMON
+moltres = Fire("Moltres", 90, 100, 90, "Ancient Power", "Fire")
+lapras = Water("Lapras", 130, 85, 60, "Hydro Pump", "Water")
+
+pokemons = [treeko, moltres, lapras]
 
 
 def choose_pokemon():
@@ -75,14 +114,18 @@ def choose_pokemon():
                       choices=[*pokemons],
                       ),
     ])
-    match poke_selected['pokes']:
-        case "Treeko":
-            poke = treeko
-        case "Moltres":
-            poke = moltres
-        case "Lapras":
-            poke = lapras
-    return poke
+    return poke_selected['pokes']
+    # match poke_selected['pokes']:
+    #     case "Treeko":
+    #         poke = treeko
+    #         return poke
+    #     case "Moltres":
+    #         poke = moltres
+    #         return poke
+    #     case "Lapras":
+    #         poke = lapras
+    #         return poke
+
 
 def poke_bot():
     pok_bot = random.choice(pokemons)
@@ -94,24 +137,33 @@ def poke_bot():
         pok_bot = lapras
     return pok_bot
 
-def choose_mov():
-    
 
+def loose_hp(player_one, player_bot):
+    atk = player_one.attack(player_bot.type, player_one.damage)
+    print(atk)
+    loose_hp_bot = player_bot.life - atk
+    print(loose_hp_bot)
 
-
-
-
-
+    return loose_hp_bot
 
 
 def gameplay():
     player_one = choose_pokemon()
     player_bot = poke_bot()
+    while player_one.life > 0 and player_bot.life > 0:
+        if player_one.velocity >= player_bot.velocity:
+            player_one.advantage(player_bot.type, player_one.attack(player_bot.type, player_one.damage))
+            player_one.choose_mov()
 
-    if player_one.velocity < player_bot:
-        ...
+            loose_hp(player_one, player_bot)
+        else:
+            print("baaba")
+
+    # if player_one.velocity < player_bot:
+    #     ...
     print(player_one)
     print(player_bot)
+    print(player_one.mov)
     # if player_one.velocity < player_bot.velocity:
     #     player_one.advantage(player_bot.type)
 
